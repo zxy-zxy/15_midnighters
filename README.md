f00797a1818185d82ba1e57a2ca038eb3160f832
ivan <sinitsinvanya@gmail.com>
2018-05-18 17:33:06 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+A python script for detecting midnighters.
 
+## Requirements
+Python >= 3.5 required. 
+
+Dependencies:
+
+1.  [Requests library](http://docs.python-requests.org/en/master/)
+2.  [Pytz](http://pytz.sourceforge.net/)
+
+For better interaction is recommended to use [virtualenv](https://github.com/pypa/virtualenv).
+
+You can easily install dependencies with 
+```bash
+pip install -r requirements.txt
+```
+### Example input
+```bash
+python seek_dev_nighters.py
+```
 # Project Goals
-
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,67 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+from requests.exceptions import RequestException
+import json
+from json import JSONDecodeError
+from datetime import datetime
+from pytz import timezone
 
-def get_midnighters():
-    pass
+
+def get_midnighters(attempt):
+    midnight_hour_limit = 3
+
+    users = set()
+
+    for attempt in attempts:
+        user_time = datetime.fromtimestamp(
+            attempt['timestamp'],
+            tz=timezone(attempt['timezone'])
+        )
+
+        if user_time.hour < midnight_hour_limit:
+            users.add(attempt['username'])
+
+    return users
+
+
+def get_response_from_page(page_number):
+    api_address = 'https://devman.org/api/challenges/solution_attempts'
+    response = requests.get(api_address, params={'page': page_number})
+    return {
+        'status_code': response.status_code,
+        'text': response.text
+    }
+
+
+def load_attempts():
+    current_page_number = 1
+
+    while True:
+        try:
+            current_page_response = get_response_from_page(current_page_number)
+        except RequestException:
+            raise StopIteration()
+
+        if current_page_response.get('status_code') != 200:
+            raise StopIteration()
+
+        try:
+            page_data = json.loads(current_page_response.get('text'))
+        except JSONDecodeError:
+            raise StopIteration()
+
+        for record in page_data['records']:
+            yield record
+
+        if current_page_number == page_data['number_of_pages']:
+            raise StopIteration()
+
+        current_page_number = current_page_number + 1
+
 
 if __name__ == '__main__':
-  pass
+    attempts = load_attempts()
+
+    midnighters = get_midnighters(attempts)
+
+    for _index, midnighter in enumerate(midnighters):
+        print("{}: {}".format(_index, midnighter))
